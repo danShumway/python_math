c9130a98000ce949e922b21d7e9d6e7dc21c7e2d
Daniel Shumway <shumway.danny@gmail.com>
2014-04-27 19:20:43 -0400

    Getting ready to merge.  Working on menus

--- source/PythonMath.activity/game/menu.py
+++ source/PythonMath.activity/game/menu.py
@@ -1,12 +1,21 @@
+import spyral
+import math
+import random
+
 class Menu():
 
-    def __init__(self):
+    def __init__(self, scene):
         #set up variables
-        #python literally does not have private or hidden variables
-        #but using __ will mangle them so they're hard to use/implied not to use
+        #python literally does not have private or hidden variables, as far as I know.
+        self.parentScene = scene #A reference to the drawy part.
         self.options = [] #what the text for each menu item is
         self.optionsDelegates = [] #what functions it points to.
         self.currentlySelected = 0 #what menu item we're on.
+        self.sprites = [] #Our sprites.
+        self.width = 0
+        self.height = 0 #width and height of the entire menu
+        self.originX = 0
+        self.originY = 0
 
 #----------------------------------------------------------------------
 
@@ -14,12 +23,24 @@ class Menu():
     def addMenuItem(self, itemText, itemDelegate):
         self.options.append(itemText)
         self.optionsDelegates.append(itemDelegate)
-
+        #add the actual sprite.  These are drawn automatically on the scene that's passed in.
+        #there is no draw loop.
+        option = spyral.Sprite(self.parentScene)
+        self.sprites.append(option)
+        option.image = spyral.Image("game/logo.png")
+        #recalculate the position of everything on the screen.
+        option.anchor = "center"
+        #option.width = self.width*.5
+        #option.height = self.height*.2
+        option.x = self.height/2 + self.originX
+        option.y = self.width/2 + self.originY
+        
 #----------------------------------------------------------------------
 
     #select a menu item.
     def selectCurrent(self):
         self.optionsDelegates[self.currentlySelected]()
+        #highlight the selected option.
     
     
     
--- source/PythonMath.activity/game/pythonMath.py
+++ source/PythonMath.activity/game/pythonMath.py
@@ -14,7 +14,7 @@ class PythonMathGame(spyral.Scene):
     def __init__(self, *args, **kwargs):
         spyral.Scene.__init__(self, SIZE)
         self.background = spyral.Image(size=SIZE).fill(BG_COLOR)
-
+        '''
         self.MainMenuLogo = spyral.Sprite(self)
         logo_image = spyral.Image("game/logo.png")
         self.MainMenuLogo.image = logo_image
@@ -23,6 +23,7 @@ class PythonMathGame(spyral.Scene):
 
         self.MainMenuLogo.x = WIDTH/2
         self.MainMenuLogo.y = HEIGHT/2
+        '''
         self.i = 0
         self.colorThing = (0, 0, 0)
 
@@ -30,12 +31,16 @@ class PythonMathGame(spyral.Scene):
 
         spyral.event.register("system.quit", spyral.director.pop)
         spyral.event.register("director.update", self.update)
-        spyral.event.register("input.keyboard.down.q", spyral.director.pop)
+        spyral.event.register("input.keyboard.down.q", spyral.director.quit)
 
 
 
         #some real stuff.  Still just hacking and testing, so not really really real.
-        self.mainMenu = menu.Menu();
+        self.mainMenu = menu.Menu(self);
+        self.mainMenu.width = WIDTH
+        self.mainMenu.height = HEIGHT
+        #self.mainMenu.originX = WIDTH/2
+        #self.mainMenu.originY = HEIGHT/2
         self.mainMenu.addMenuItem("hello", self.moveSprite)
         self.currentlySelected = 0
         spyral.event.register("input.keyboard.down.a", self.mainMenu.selectCurrent)
@@ -54,7 +59,8 @@ class PythonMathGame(spyral.Scene):
         #self.MainMenuLogo.x = self.MainMenuLogo.x + 1
 
     def moveSprite(self):
-        self.MainMenuLogo.x += 2
+        #self.MainMenuLogo.x += 2
+        pass
  
         
     
