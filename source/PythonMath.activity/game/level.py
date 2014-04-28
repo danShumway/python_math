06cd26ccd523ca43e80e63250cd23a7116fb29f6
Mike Tubinis <michaeltubinis@gmail.com>
2014-04-28 11:48:59 -0400

    Update level.py
    forgot to remove 2 testing things

--- source/PythonMath.activity/game/level.py
+++ source/PythonMath.activity/game/level.py
@@ -10,8 +10,8 @@ class Tile(spyral.Sprite):
              self.image = spyral.Image(size=(32,32)).fill((0,0,0))
         self.anchor = 'center'
 
-        self.x = (i * 1.05) * self.image.width + SIZE[0]/4
-        self.y = (j * 1.05) * self.image.height + SIZE[1]/4
+        self.x = i * self.image.width + SIZE[0]/4
+        self.y = j * self.image.height + SIZE[1]/4
 
 class Level(spyral.Scene):
     def __init__(self,SIZE):
