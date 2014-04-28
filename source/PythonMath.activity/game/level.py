430cb8d7c0cafe682be9d6653b5b2691616b6e0c
Mike Tubinis <michaeltubinis@gmail.com>
2014-04-28 16:04:42 -0400

    added pan camera functionality to level

--- source/PythonMath.activity/game/level.py
+++ source/PythonMath.activity/game/level.py
@@ -1,4 +1,6 @@
 import spyral
+import gameInput
+from gameInput import InputClass
 
 class Tile(spyral.Sprite):
     def __init__(self, scene, i, j,SIZE,key):
@@ -10,20 +12,24 @@ class Tile(spyral.Sprite):
              self.image = spyral.Image(size=(32,32)).fill((0,0,0))
         self.anchor = 'center'
 
-        self.x = i * self.image.width + SIZE[0]/2 - (scene.levelWidth/2 * self.image.width)
-        self.y = j * self.image.height + SIZE[1]/2 - (scene.levelWidth/2 * self.image.height)
+        self.x = (i*0.95) * self.image.width + SIZE[0]/2 - (scene.levelWidth/2 * self.image.width)
+        self.y = (j*0.95) * self.image.height + SIZE[1]/2 - (scene.levelWidth/2 * self.image.height)
+
 
 class Level(spyral.Scene):
     def __init__(self,SIZE):
         spyral.Scene.__init__(self, SIZE)
-        self.background = spyral.Image(size=SIZE).fill((75,255,75))
-        
+        self.background = spyral.Image(size=SIZE).fill((25,150,25))
+
+        self.sceneSize = SIZE
         self.levelWidth = 20;
         self.levelHeight = 20;
         self.levelData = self.CreateLevel(SIZE,'game/level.txt')
 
         spyral.event.register("director.update", self.update)
         spyral.event.register("input.keyboard.down.q", spyral.director.pop)
+
+        InputClass.RegisterEvents()
 
     #makes a blank world and returns it.
     #Todo: load in worlds from external files.
@@ -67,7 +73,24 @@ class Level(spyral.Scene):
         return level
 
     def update(self, delta):
-        pass
+        InputClass.Update(delta)
+        self.PanCamera()
 
     def GetTile(self, row, column):
         return self.levelData[((row-1) * self.levelWidth) + (column-1)]
+
+    def PanCamera(self):
+        xOffset = 0
+        yOffset = 0
+        if InputClass.mouseData.x <= 15:
+            xOffset = 3
+        elif InputClass.mouseData.x >= self.sceneSize[0] - 15:
+            xOffset = -3
+        if InputClass.mouseData.y <= 15:
+            yOffset = 3
+        elif InputClass.mouseData.y >= self.sceneSize[1] - 15:
+            yOffset = -3
+        if yOffset != 0 or xOffset != 0:
+            for i in self.levelData:
+                i.x += xOffset
+                i.y += yOffset
